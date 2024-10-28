e17cc15b3c820b95fb6cd88bdbcb750627097974
Jun Su Lee <45049935+junsulee75@users.noreply.github.com>
2024-10-28 23:03:22 +1100

    waitDpkgLockRelease

--- installKube.sh
+++ installKube.sh
@@ -3,6 +3,38 @@
 source config.ini # use /bin/bash for reading from the current directory
 source jscommon.sh
 
+case "$version_id" in
+    "24.04"|"Some future version to add")
+        echo "Wait for some time before creating kubenetes cluster....";sleep 120
+        ;;
+    *)
+        echo "continue without sleep !!! "
+        ;;
+
+esac
+
+# kubemetal_002 : common function to wait until there is no lock on the file "/var/lib/dpkg/lock-frontend"   
+# To avoid the error "Could not get lock /var/lib/dpkg/lock-frontend. It is held by process 3199 (unattended-upgr)"  
+
+waitDpkgLockRelease() {
+    
+    remote_host="$1"
+    
+    while true; do
+        ssh $SSH_NO_BANNER $remote_host "lsof 2>/dev/null | grep /var/lib/dpkg/lock-frontend"
+        lockCnt = $(ssh $SSH_NO_BANNER $remote_host "lsof 2>/dev/null | grep /var/lib/dpkg/lock-frontend | wc -l")
+        
+        if [[ "$lockCnt" -eq 0 ]]; then
+            echo "$remote_host : No lock on the file /var/lib/dpkg/lock-frontend. Continue ..."
+            break
+        else
+            echo "$remote_host : $lockCnt locks on the file /var/lib/dpkg/lock-frontend. Wait for 5 seconds and try again !!"
+        fi
+    done
+
+}
+
+
 addGPGKey() {
 
     print1 "Install kubeadm, kubelet and kubectl (Reference : https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/  ) "   
@@ -10,6 +42,8 @@ addGPGKey() {
     for i in $ALLHOST
     do
 
+        print2 "$i : check file lock on /var/lib/dpkg/lock-frontend by lsof on the target systems. "
+        waitDpkgLockRelease "${i}"
         print2 "$i : Add GPG key"
         ssh $SSH_NO_BANNER $HOST  ${i} "sudo apt-get update -y"
         # apt-transport-https may be a dummy package; if so, you can skip that package
@@ -37,6 +71,8 @@ installKube() {
 
     for i in $ALLHOST
     do
+        print2 "$i : check file lock on /var/lib/dpkg/lock-frontend by lsof on the target systems. "
+        waitDpkgLockRelease "${i}"
         print2 "$i : Install kubelet kubeadm kubectl"
         ssh $SSH_NO_BANNER $HOST  ${i} "sudo apt-get install -y kubelet kubeadm kubectl"
         ssh $SSH_NO_BANNER $HOST  ${i} "sudo apt-mark hold kubelet kubeadm kubectl"
